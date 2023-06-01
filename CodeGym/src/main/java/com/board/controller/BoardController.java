341f2259c6d8ad712ed0111925cf8a57ff5ac5c8
“dolbi7” <freeafwan0@gmail.com>
2023-06-01 15:44:47 +0900

    컨트롤러 수정

--- CodeGym/src/main/java/com/board/controller/BoardController.java
+++ CodeGym/src/main/java/com/board/controller/BoardController.java
@@ -1,12 +1,8 @@
 package com.board.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
-//	@GetMapping("/user/signup")
-//	public void getSignup() {
-//	}
 }
