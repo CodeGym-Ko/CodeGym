df19ad042dc3ce63202a1e3cf88b888e56e82903
tkdudgns95 <tkdudgns95@naver.com>
2023-06-12 16:03:08 +0900

    부위 별 운동 페이지 (스트레칭,맨몸운동, 프리웨이트) 추가 완료.

--- CodeGym/src/main/java/com/board/controller/ExerciseController.java
+++ CodeGym/src/main/java/com/board/controller/ExerciseController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ExerciseController {
-	
+	/* <프리웨이트 시작> */
 	@GetMapping("/exercise/freeWeight/index")
 	public void getIndex() {
 		
@@ -16,9 +16,255 @@ public class ExerciseController {
 
 	}
 
-	@GetMapping("/exercise/abdominals")
+	
+	@GetMapping("/exercise/abdominals") 
 	public void getAbs() {
+	
+	  }
+	 
+	
+	
+	@GetMapping("/exercise/freeWeight/abdominals3")
+	public void getabdominals() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Obliques3")
+	public void getObliques() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/ForeArms3")
+	public void getForeArms() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Biceps3")
+	public void getBiceps() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Shoulder3")
+	public void getShoulder() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/traps3")
+	public void gettraps() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Quads3")
+	public void getQuads() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Calves3")
+	public void getCalves() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Triceps3")
+	public void getTriceps() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/TrapsMid3")
+	public void getTrapsMid() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Lats3")
+	public void getLats() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Lowerback3")
+	public void Lowerback() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/Glutes3")
+	public void Glutes() {
+
+	}
+	
+	@GetMapping("/exercise/freeWeight/HarmStrings3")
+	public void HarmStrings() {
+
+	}
+	/* <프리웨이트 끝> */
+	
+	/* <스트레칭 시작> */
+	@GetMapping("/exercise/stretching/index")
+	public void getIndex1() {
+		
+	}
+	
+	@GetMapping("/exercise/stretching/Chest1")
+	public void getChest1() {
 
 	}
 
+	
+	@GetMapping("/exercise/stretching/Abdominals1") 
+	public void getAbdominals1() {
+	
+	  }
+	 
+	@GetMapping("/exercise/stretching/Obliques1")
+	public void getObliques1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/ForeArms1")
+	public void getForeArms1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/Biceps1")
+	public void getBiceps1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/Shoulder1")
+	public void getShoulder1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/traps1")
+	public void gettraps1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/Quads1")
+	public void getQuads1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/Calves1")
+	public void getCalves1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/Triceps1")
+	public void getTriceps1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/TrapsMid1")
+	public void getTrapsMid1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/Lats1")
+	public void getLats1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/Lowerback1")
+	public void Lowerback1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/Glutes1")
+	public void Glutes1() {
+
+	}
+	
+	@GetMapping("/exercise/stretching/HarmStrings1")
+	public void HarmStrings1() {
+
+	}
+
+	
+	/* <스트레칭 끝> */
+	
+	
+	/* <맨몸운동 시작> */
+	@GetMapping("/exercise/bodyWeight/index")
+	public void getIndex2() {
+		
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Chest2")
+	public void getChest2() {
+
+	}
+
+	
+	@GetMapping("/exercise/bodyWeight/Abdominals2") 
+	public void getAbdominals2() {
+	
+	  }
+	 
+	@GetMapping("/exercise/bodyWeight/Obliques2")
+	public void getObliques2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/ForeArms2")
+	public void getForeArms2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Biceps2")
+	public void getBiceps2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Shoulder2")
+	public void getShoulder2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/traps2")
+	public void gettraps2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Quads2")
+	public void getQuads2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Calves2")
+	public void getCalves2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Triceps2")
+	public void getTriceps2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/TrapsMid2")
+	public void getTrapsMid2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Lats2")
+	public void getLats2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Lowerback2")
+	public void Lowerback2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/Glutes2")
+	public void Glutes2() {
+
+	}
+	
+	@GetMapping("/exercise/bodyWeight/HarmStrings2")
+	public void HarmStrings2() {
+
+	}
+	
+	
+	/* <맨몸운동 끝> */
+	
+	
 }
