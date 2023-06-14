eeeac904ef3103b56bbde71d4f8d82a5b7501c58
aaa <aaa@DESKTOP-5AFS6S9>
2023-06-14 13:59:42 +0900

    이거 아님

--- CodeGym/src/main/java/com/board/controller/NutritionController.java
+++ CodeGym/src/main/java/com/board/controller/NutritionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class NutritionController {
 
-	@GetMapping("/nutritional/bulkupTest")
+	@GetMapping("/nutritional/bulkup")
 	public void getBulkup() {
 		
 	}
