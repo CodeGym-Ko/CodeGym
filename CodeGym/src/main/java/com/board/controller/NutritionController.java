1c7f807f047288f06ae7f86d282f11f752822454
aaa <aaa@DESKTOP-5AFS6S9>
2023-06-14 13:56:52 +0900

    삽질

--- CodeGym/src/main/java/com/board/controller/NutritionController.java
+++ CodeGym/src/main/java/com/board/controller/NutritionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class NutritionController {
 
-	@GetMapping("/nutritional/bulkup")
+	@GetMapping("/nutritional/bulkupTest")
 	public void getBulkup() {
 		
 	}
